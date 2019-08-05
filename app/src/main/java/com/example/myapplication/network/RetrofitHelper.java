0f81eb29dfd22ca1cbddf0db172d46915766eb70
JaekangLee <ksdy234@gmail.com>
2019-08-05 21:31:31 +0900

    ADD [ Retrofit Helper Class ] -  레트로핏 헬퍼 클래스 -> 기본 세팅

---
+++ app/src/main/java/com/example/myapplication/network/RetrofitHelper.java
@@ -0,0 +1,46 @@
+package com.example.myapplication.network;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitHelper {
+
+
+    public static RetrofitHelper retrofitHelper = new RetrofitHelper();
+    public Retrofit retrofit;
+
+    public static RetrofitHelper getinstance() {
+        return retrofitHelper;
+    }
+
+    public RetrofitHelper() {
+        logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClient = new OkHttpClient.Builder();
+        httpClient.connectTimeout(5, TimeUnit.SECONDS);
+        httpClient.readTimeout(5, TimeUnit.SECONDS);
+        httpClient.writeTimeout(5, TimeUnit.SECONDS);
+        httpClient.addInterceptor(logging);/*
+        httpClient.addInterceptor(new AddCookiesInterceptor());
+        httpClient.addInterceptor(new ReceivedCookiesInterceptor());*/
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https:www.naver.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+    }
+
+    private HttpLoggingInterceptor logging;
+    private OkHttpClient.Builder httpClient;
+
+
+    public  SignupApi getSignupApi() {
+        return retrofit.create(SignupApi.class);
+    }
+
+}
